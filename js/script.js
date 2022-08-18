3b529bca918de6832017a74821c1e26782691cca
Иззатилло Турсунов <izzatillotursunov@MacBook-Pro-Izzatillo.local>
2022-08-18 15:57:25 +0500

    got all the values, it ramains to finish

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded' ,() =>{
+  //tabs
 const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector('.tabheader__items');
@@ -31,8 +32,49 @@ const tabs = document.querySelectorAll('.tabheader__item'),
         });
       } 
       });
+     
+
+      //timer
+     const deadLine = "2020-05-11";
+     function getTimeRemaining(endtime){
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+            day = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor( (t / 1000 / 60) % 60),
+            seconds = Math.floor( (t /  1000 ) % 60);
+
+
+            return {
+              'total':t,
+              'days':day,
+              'hours':hours,
+              'minutes':minutes,
+              'seconds':seconds
+             };
+     }
+     function setClock(selector , endtime){
+      const timer = document.querySelector('selector'),
+             days = timer.querySelector("#days"),
+             hours = timer.querySelector("#hours"),
+             minutes = timer.querySelector("#minutes"),
+             seconds = timer.querySelector("#seconds"),
+             timeInterval = setInterval(updateClock(), 1000);
+             //upload timer every seconds
+             function updateClock(){
+              const t = getTimeRemaining(endtime);
+              days.innerHTML = t.days;
+              hours.innerHTML = t.hours;
+              minutes.innerHTML = t.minutes;
+              seconds.innerHTML = t.seconds;
+              if(t.total <= 0){
+                clearInterval(timeInterval);
+              }
+             }
+     }
 });
 
+
+//tabs
 // document.addEventListener('DOMContentLoaded',() =>{
 //     const tabs = document.querySelectorAll('.tabheader__item'),
 //           tabsContent = document.querySelectorAll('.tabcontent'),
@@ -64,3 +106,6 @@ const tabs = document.querySelectorAll('.tabheader__item'),
 //           } 
 //           });
 //     });
+
+
+
