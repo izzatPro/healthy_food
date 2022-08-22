bb8dc768daa44961f339303635703500040bcab1
Иззатилло Турсунов <izzatillotursunov@MacBook-Pro-Izzatillo.local>
2022-08-22 20:03:26 +0500

    rest update to card

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 document.addEventListener('DOMContentLoaded' ,() =>{
   //tabs
 const tabs = document.querySelectorAll('.tabheader__item'),
@@ -139,7 +140,82 @@ const tabs = document.querySelectorAll('.tabheader__item'),
 
         window.addEventListener('scroll', showModalByScroll);
 
-    j
+    //Используем классы для карточек
+
+      class MenuCard {
+        //принимает содержимое карточек
+        constructor(src,alt,title,descr,price,parentSelector, ...classes){
+          this.src = src;
+          this.alt = alt;
+          this.title = title;
+          this.descr = descr;
+          this.price = price;
+          this.classes = classes;
+          this.parent = document.querySelector(parentSelector);
+          this.tranfer = 27;
+          this.changeToUAH();
+        }
+        //гривины в доллары переводит
+        changeToUAH(){
+          this.price = this.price * this.tranfer;
+        }
+        //формирует верстку
+        render(){
+        const element = document.createElement('div');
+        if(this.classes.length == 0){
+        this.defaultClass = 'menu__item';
+        element.classList.add(this.defaultClass);
+        }
+        else{
+          this.classes.forEach(className => element.classList.add(className));
+        }
+        element.innerHTML+=`              
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>`;
+          this.parent.append(element);
+        }
+      }
+
+      //Если нам нужно использовать объект один раз
+      //Мы можем не создавать переменную для неё
+
+      new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+  '      Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container',
+      ).render();
+
+
+
+
+      new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+     '   В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!' , 
+        9,
+        '.menu .container',
+        
+      ).render();
+
+      new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+  '     Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+        9,
+        '.menu .container',
+        
+      ).render();
 
 
 
